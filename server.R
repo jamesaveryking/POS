05e7655e196277ae54129bfdecc0f8d3b170434c
James Avery King <jamesaveryking@Jamess-MacBook-Air.local>
2019-04-06 21:50:22 -0400

    Initial Commit of POS system in R Shiny

---
+++ server.R
@@ -0,0 +1,42 @@
+
+
+# Define server logic to read selected file ----
+server <- function(input, output) {
+  
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+        
+        output$revenueField<-renderText(sum(df$SALES))
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
+    
+  })
+  
+  
+}
